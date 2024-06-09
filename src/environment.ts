e68f425f611ca16d539d7e2e109647c2f3f0d217
Suman Tamang <sumantamang1020@gmail.com>
2024-06-09 15:41:03 +0930

    added env file

---
+++ src/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyCmC4-UQmRnJtFtErDDNqJ8KH8fEvEZoWU",
+      authDomain: "videotranslator-55acc.firebaseapp.com",
+      projectId: "videotranslator-55acc",
+      storageBucket: "videotranslator-55acc.appspot.com",
+      messagingSenderId: "991172985524",
+      appId: "1:991172985524:web:31427df4e17c2e43f58de1",
+      measurementId: "G-SYQDV064YE"
+    }
+  };
+  
