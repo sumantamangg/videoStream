04b3d545f1629e2425ed74ab4a7b605f329f6247
Suman Tamang <sumantamang1020@gmail.com>
2024-06-13 15:37:23 +0930

    video playback from dynamic path fixed

--- src/app/pages/video-library/video-library.component.ts
+++ src/app/pages/video-library/video-library.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { map } from 'rxjs/operators';
 import { AuthService } from '../../services/auth.service';
@@ -13,6 +13,9 @@ export class VideoLibraryComponent implements OnInit{
 
   videoUrl: any = "";
   currentUser: any = null;
+  @ViewChild('videoPlayer') videoPlayer: ElementRef<HTMLVideoElement>;
+  currentTime = 0;
+  isPlaying = false;
 
   constructor(private firestore: AngularFirestore,
               private authService: AuthService
@@ -25,16 +28,33 @@ export class VideoLibraryComponent implements OnInit{
   }
 
   ngOnInit(): void {
-  
     this.firestore.collection(this.currentUser.uid).valueChanges() // Use valueChanges()
       .subscribe(videos => {
         if (videos.length > 0) {
           this.videoUrl = videos[0]; // get the first video
+          console.log("irl after subscribe =", this.videoUrl.url); // Updated videoUrl after fetching
         } else {
           console.log('No video found for user');
         }
-        console.log("irl after subscribe =", this.videoUrl.url); // Updated videoUrl after fetching
-      });
+    });
+  }
+
+  onLoadedMetadata(e) {
+    this.videoPlayer.nativeElement.currentTime = this.currentTime; // Set initial position if any
+    this.videoPlayer.nativeElement.setAttribute('max', String(e.currentTarget.duration));
+  }
+
+  onSeek(e) {
+    this.videoPlayer.nativeElement.currentTime = e.target.value;
+  }
+
+  playPause() {
+    this.isPlaying = !this.isPlaying;
+    if (this.isPlaying) {
+      this.videoPlayer.nativeElement.play();
+    } else {
+      this.videoPlayer.nativeElement.pause();
+    }
   }
   
 }
